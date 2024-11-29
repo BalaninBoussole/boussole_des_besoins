be6637b82d3d4a2dc63d5c11519ec7b1d8c0e5f8
BalaninBoussole <maxime.balanin@proton.me>
2024-11-29 16:04:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function answerQuestion(response) {
+    // Logique pour orienter la boussole en fonction de la réponse
+    if (response === 1) {
+        document.getElementById('need1').style.backgroundColor = '#0f0';
+    } else if (response === 2) {
+        document.getElementById('need2').style.backgroundColor = '#0f0';
+    }
+    // Ajoutez plus de logique pour orienter la boussole
+}
